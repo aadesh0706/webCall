fcb1194ee1c8c939d5131efe82ede4701b5711d8
Aadesh Gulumbe <71577243+aadesh0706@users.noreply.github.com>
2024-06-03 16:11:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ io.on('connection', function (socket) {
 
 });
 
-http.listen(3000, function () {
+http.listen('https://web-call-6x2n.vercel.app/', function () {
     console.log('listening on *:3000');
 console.log("http://localhost:3000");
 });
