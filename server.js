a3a6ba6b03708d728797e6f31faa9365837af3a1
Aadesh Gulumbe <71577243+aadesh0706@users.noreply.github.com>
2024-12-11 19:52:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 const path = require("path");
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, "public")));
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 io.on('connection', function (socket) {
     socket.on('create', function (callback) {
